2cab409694c75809ff305759c9080f5b765fd995
Leon <yonnn1007@gmail.com>
2025-05-31 14:00:05 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT || 3000,
+  port: process.env.DB_PORT || 3306,
 });
 
 connection.connect((err) => {
