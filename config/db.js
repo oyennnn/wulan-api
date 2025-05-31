d8befd1cc8ca94f21c88ce6e04fad8a80e731e71
Leon <yonnn1007@gmail.com>
2025-05-31 11:41:24 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
 });
 
 connection.connect((err) => {
